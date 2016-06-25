b3df7928591007c9e2b1d3ddb16d8a1422de4cb4
Will Humphreys <whumphreys@gmail.com>
2016-06-25 07:04:25 +0100

    Need to link the button on the add item to the controller.

---
+++ components/classifieds.ctr.js
@@ -0,0 +1,23 @@
+(function () {
+    "use strict";
+
+    angular.module("ngClassifieds")
+        .controller("classifieds.Ctrl", function ($scope, $http, classifiedsFactory, $mdSidenav) {
+
+            classifiedsFactory.getClassifieds().then(function (classifieds) {
+                $scope.classifieds = classifieds.data;
+            });
+
+            $scope.openSidebar = function() {
+                $mdSidenav('left').open();
+            };
+
+            $scope.closeSidebar = function() {
+                $mdSidenav('left').close();
+            };
+
+            $scope.saveClassified = function(classified) {
+                $scope.classifieds.push(classified);
+            }
+        });
+})();
