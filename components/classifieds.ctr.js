b29d402da9970602061dd8800b9dcf74391114a2
Will Humphreys <whumphreys@gmail.com>
2016-06-26 07:57:16 +0100

    Add a message when we save a listing. Add a dialog when we delete an item.

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -2,7 +2,7 @@
     "use strict";
 
     angular.module("ngClassifieds")
-        .controller("classifieds.Ctrl", function ($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
+        .controller("classifieds.Ctrl", function ($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 
             classifiedsFactory.getClassifieds().then(function (classifieds) {
                 $scope.classifieds = classifieds.data;
@@ -28,13 +28,39 @@
                     $scope.classifieds.push(classified);
                     $scope.classified = {};
                     $scope.closeSidebar();
-                    $mdToast.show($mdToast.simple().content("Classified Saved!").position("top, right").hideDelay(3000))
+                    showToast("Classified saved!");
                 }
             };
 
             $scope.editClassified = function (classified) {
                 $scope.editing = true;
                 $scope.openSidebar();
+                $scope.classified = classified;
             };
+
+            $scope.saveEdit = function () {
+                $scope.editing = false;
+                $scope.classified = {};
+                $scope.closeSidebar("Edit saved!");
+            };
+
+            $scope.deleteClassified = function (classified, event) {
+                var confirm = $mdDialog.confirm()
+                    .title('Are you sure you want to delete ' + classified.title + '?')
+                    .ok('Yes')
+                    .cancel('No')
+                    .targetEvent(event);
+                $mdDialog.show(confirm).then(function() {
+                    var index = $scope.classifieds.indexOf(classified);
+                    $scope.classifieds.splice(index, 1);
+                }, function() {
+
+                });
+            };
+
+            function showToast(message) {
+                $mdToast.show($mdToast.simple().content(message).position("top, right").hideDelay(3000))
+            }
+
         });
 })();
