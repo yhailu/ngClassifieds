e56a1cc1afa9739eb51b9a29ef5c1f500c046af2
Will Humphreys <whumphreys@gmail.com>
2016-06-27 05:52:46 +0100

    Start learning at routing. Add some dummy routes.

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,22 @@
 angular
-    .module("ngClassifieds", ["ngMaterial"])
-    .config(function ($mdThemingProvider) {
+    .module("ngClassifieds", ["ngMaterial", "ui.router"])
+    .config(function ($mdThemingProvider, $stateProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('teal')
             .accentPalette('orange');
-    }).directive("helloWorld", function () {
-        return {
-            template: "<h1>{{ message }}</h1>"
-        }
+        $stateProvider.state('stateone', {
+            url: '/stateone',
+            template: '<h1>{{ stateone.message }}</h1>',
+            controller: 'stateOneCtrl as stateone'
+        }).state('statetwo', {
+            url: '/statetwo',
+            template: '<h1>State Two</h1> <md-button ui-sref="more">Go to nested state</md-button>'
+        }).state('more', {
+            url: '/more',
+            template: '<p>This is the deeper state</p>'
+        });
+    }).controller('stateOneCtrl', function () {
+    var vm = this;
+    vm.message = "Hey from state one";
+
 });
