deb413693964892243af1f225148684ddd242b19
Will Humphreys <whumphreys@gmail.com>
2016-06-28 06:17:04 +0100

    Get the new classified routing working.

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,10 @@ angular
         $stateProvider.state('classifieds', {
             url: '/classifieds',
             templateUrl: 'components/classifieds/classifieds.tpl.html',
-            controller: 'classifieds.Ctrl as vm'
+            controller: 'classifiedsCtrl as vm'
+        }).state('classifieds.new', {
+            url: '/new',
+            templateUrl: 'components/classifieds/new/classifieds.new.tpl.html',
+            controller: 'newClassifiedsCtrl as vm'
         })
     });
