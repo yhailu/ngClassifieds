3df3e1db360616b04a68982cf85f00d45be89b05
Will Humphreys <whumphreys@gmail.com>
2016-06-29 06:11:21 +0100

    Get the new classified state working on cancel

--- components/classifieds/new/classifieds.new.ctr.js
+++ components/classifieds/new/classifieds.new.ctr.js
@@ -1,11 +1,29 @@
-(function() {
-   "use strict";
+(function () {
+    "use strict";
 
-    angular.module('ngClassifieds').controller('newClassifiedsCtrl', function($mdSidenav, $timeout, $mdDialog, classifiedsFactory){
+    angular.module('ngClassifieds').controller('newClassifiedsCtrl', function ($scope, $state, $mdSidenav, $timeout, $mdDialog, classifiedsFactory, $mdComponentRegistry) {
         var vm = this;
+        vm.closeSidebar = closeSidebar;
 
-        $timeout(function() {
-            $mdSidenav('left').open();
+        $timeout(function () {
+            $mdComponentRegistry.when('left').then(function () {
+                $mdSidenav('left').open();
+            });
+
         });
+
+        $scope.$watch('vm.sidenavOpen', function (sidenav) {
+            if (sidenav === false) {
+                $mdComponentRegistry.when('left').then(function () {
+                    $mdSidenav('left').close().then(function () {
+                        $state.go('classifieds');
+                    });
+                });
+            }
+        });
+
+        function closeSidebar() {
+            vm.sidenavOpen = false;
+        }
     })
 })();
